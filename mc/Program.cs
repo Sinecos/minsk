e0d9d71ac671f5f62d6780943c1703ee59abbb16
Sinecos <zufa.altake@gmail.com>
2023-02-02 01:06:31 +0200

    parse
    parsing

--- mc/Program.cs
+++ mc/Program.cs
@@ -2,29 +2,66 @@
 
 namespace mc
 {
-    //https://youtu.be/wgHIkdUQbp0?list=PLRAdsfhKI4OWNOSfS7EUu5GRAVmze1t2y&t=1350
+    https://youtu.be/wgHIkdUQbp0?list=PLRAdsfhKI4OWNOSfS7EUu5GRAVmze1t2y&t=2439
+ 
+    // 1 + 2 * 3
+    // 
+    //     +
+    //    / \
+    //   1  *
+    //     / \
+    //    2   3
+
+    // 1 + 2 + 3
+    // 
+    //     +
+    //    / \
+    //   +   3
+    //  / \
+    // 1   2
+ 
     class Program{
         static void Main(string[] args){
             while (true){
 
                 Console.Write("> ");
-
                 var line = Console.ReadLine();
 
                 if(string.IsNullOrWhiteSpace(line))
                     return;
 
-                if(line == "1 + 2 * 3")
-                    Console.WriteLine("7");
-                else
-                    Console.WriteLine("ERROR: Invalid expression!");
+                var lexer = new Lexer(line);
+
+                while(true){
+                    
+                    var token = lexer.NextToken();
+                    if(token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+
+                    Console.Write($"{token.Kind}: '{token.Text}'");
+                    if(token.Value != null)
+                        Console.Write($" {token.Value}");
+                        
+                    Console.WriteLine();
+                }
             }
         }
     }
 
     enum SyntaxKind
     {
-        NumberToken
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken,
+        NumberExpression,
+        BinaryExpression
     }
 
     class SyntaxToken
@@ -34,6 +71,7 @@ namespace mc
             Kind = kind;
             Position = position;
             Text = text;
+            Value = value;
        }  
 
        public SyntaxKind Kind {get; }
@@ -72,6 +110,9 @@ namespace mc
             // + - *
             // <whitespace>
 
+            if(_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+
             if(char.IsDigit(Current))
             {
                 var start = _position;
@@ -84,8 +125,106 @@ namespace mc
                 int.TryParse(text, out var value);
                 return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
             }
+
+            if(char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+
+                while(char.IsWhiteSpace(Current))
+                    Next();
+
+                var lenght = _position - start;
+                var text = _text.Substring(start, lenght);
+                return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
+            }
+
+            if(Current == '+'){
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            }
+            else if(Current == '-'){
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            }
+            else if(Current == '*'){
+                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+            }
+            else if(Current == '/'){
+                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+            }
+            else if(Current == '('){
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+            }
+            else if(Current == ')'){
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+            }
+
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+    }
+
+    abstract class SyntaxNode {
+        public abstract SyntaxKind Kind {get; }
+    }
+
+    abstract class ExpressionSyntax : SyntaxNode
+    {
+        
+    }
+
+    sealed class NumberExpressionSyntax : ExpressionSyntax
+    {
+        public NumberExpressionSyntax(SyntaxToken numberToken){
+            NumberToken = numberToken;
+        }
+        
+        public override SyntaxKind Kind => SyntaxKind.NumberExpression;
+        public SyntaxToken NumberToken {get; }
+    }
+
+    sealed class BinaryExpressionSyntax : ExpressionSyntax{
+
+        public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxNode opertorToken, ExpressionSyntax right){
+            Left = left;
+            OperatorToken = opertorToken;
+            Right = right;
         }
 
-        if(char.Is)
+        public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
+        public ExpressionSyntax Left { get; }
+        public SyntaxNode OperatorToken { get; }
+        public ExpressionSyntax Right { get; }
+    }
+
+
+    class Parser
+    {
+        private readonly SyntaxToken[] _tokens;
+        private int _position;
+
+        public Parser(string text){
+            var tokens = new List<SyntaxToken>();
+            
+            var lexer = new Lexer(text);
+            SyntaxToken token;
+            do{
+                token = lexer.NextToken();
+
+                if(token.Kind != SyntaxKind.WhitespaceToken && token.Kind != SyntaxKind.BadToken){
+                    tokens.Add(token);
+                }
+
+            } while(token.Kind != SyntaxKind.EndOfFileToken);
+
+            _tokens = tokens.ToArray();
+        }
+
+        private SyntaxToken Peek(int offset){
+            var index = _position + offset;
+            if(index >= _tokens.Length)
+                return _tokens[_tokens.Length - 1];
+           
+            return _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
     }
 }
